a3231035278a8fafe4d681022bddf837e80131f6
achin6799 <abyconspiracy@gmail.com>
2014-10-27 19:55:47 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,11 +3,9 @@ $a='yes';
 $b='no';
 if($a=='yes'){
   echo "i think i am going onto the right direction";
-  
-}elseif($b=='no'){
-  echo "I think i still need to study about creating the repositary with GIT"
-  
-  
 }
+if($b=='no'){
+  echo "I think i still need to study about creating the repositary with GIT";
+  }
 
 ?>

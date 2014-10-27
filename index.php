012d1e0e6ee75e57d2bf2442f95beb11c99a101b
achin6799 <abyconspiracy@gmail.com>
2014-10-27 19:52:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
-
-echo "My first git compiler"; 
+$a='yes';
+$b='no';
+if($a=='yes'){
+  echo "i think i am going onto the right direction";
+  
+}elseif($b=='no'){
+  echo "I think i still need to study about creating the repositary with GIT"
+  
+  
+}
 
 ?>
